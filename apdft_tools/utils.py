5809ec6e470c2305717f3f3f1843328c5fcc7c7f
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2021-10-29 00:16:06 -0400

    Preliminary error cancellation analysis

--- apdft_tools/utils.py
+++ apdft_tools/utils.py
@@ -99,6 +99,27 @@ atom_states = {
     'ar': {0: (1, 3), 1: (2, 4), 2: (3, 1)},
 }
 
+element_to_z = {
+    'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 'F': 9,
+    'Ne': 10, 'Na': 11, 'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 'S': 16,
+    'Cl': 17, 'Ar': 18, 'K': 19, 'Ca': 20, 'Sc': 21, 'Ti': 22, 'V': 23,
+    'Cr': 24, 'Mn': 25, 'Fe': 26, 'Co': 27, 'Ni': 28, 'Cu': 29, 'Zn': 30,
+    'Ga': 31,'Ge': 32, 'As': 33, 'Se': 34, 'Br': 35, 'Kr': 36, 'Rb': 37,
+    'Sr': 38, 'Y': 39, 'Zr': 40, 'Nb': 41, 'Mo': 42, 'Tc': 43, 'Ru': 44,
+    'Rh': 45, 'Pd': 46, 'Ag': 47, 'Cd': 48, 'In': 49, 'Sn': 50, 'Sb': 51,
+    'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55, 'Ba': 56, 'La': 57, 'Ce': 58,
+    'Pr': 59, 'Nd': 60, 'Pm': 61, 'Sm': 62, 'Eu': 63, 'Gd': 64, 'Tb': 65,
+    'Dy': 66, 'Ho': 67, 'Er': 68, 'Tm': 69, 'Yb': 70, 'Lu': 71, 'Hf': 72,
+    'Ta': 73, 'W': 74, 'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 'Au': 79,
+    'Hg': 80, 'Tl': 81, 'Pb': 82, 'Bi': 83, 'Po': 84, 'At': 85, 'Rn': 86,
+    'Fr': 87, 'Ra': 88, 'Ac': 89, 'Th': 90, 'Pa': 91, 'U': 92, 'Np': 93,
+    'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97, 'Cf': 98, 'Es': 99, 'Fm': 100,
+    'Md': 101, 'No': 102, 'Lr': 103, 'Rf': 104, 'Db': 105, 'Sg': 106,
+    'Bh': 107, 'Hs': 108, 'Mt': 109, 'Ds': 110, 'Rg': 111, 'Cn': 112,
+    'Uuq': 114, 'Uuh': 116,
+}
+element_to_z = {key.lower():value for key,value in element_to_z.items()}
+
 def read_json(json_path):
     """Read JSON file.
     
@@ -116,6 +137,25 @@ def read_json(json_path):
         json_dict = json.load(reader)
     
     return json_dict
+
+def system_to_atomic_numbers(system_label):
+    """
+
+    Parameters
+    ----------
+    system_label : :obj:`str`
+        Specifies the system by stringing together element symbols with a
+        ``'.'`` joining them.
+    
+    Returns
+    -------
+    :obj:`numpy.ndarray`
+        Atomic numbers in the system in the same order as the system_label.
+    """
+    atomic_numbers = np.array(
+        [element_to_z[i] for i in system_label.split('.')]
+    )
+    return atomic_numbers
 
 def get_lambda_value(
     ref_atomic_numbers, target_atomic_numbers, specific_atom=None,
